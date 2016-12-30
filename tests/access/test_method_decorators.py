3c4dfa2245d4c92377c91911f120397c83cbd11f
Wayne Warren <waynr+launchpad@sdf.org>
2016-12-29 21:59:31 -0800

    Use scenario based testing for decorator tests.

--- tests/access/test_method_decorators.py
+++ tests/access/test_method_decorators.py
@@ -56,94 +56,10 @@ class Sub(Base):
 class TestInstanceMethodAccessControl(testtools.TestCase):
 
     def setUp(self):
-        self.baseclassaseclass = Base()
+        self.bbaseclassaseclassaseclass = Base()
         self.subclass = Sub()
         super(TestInstanceMethodAccessControl, self).setUp()
 
     def tearDown(self):
         super(TestInstanceMethodAccessControl, self).tearDown()
 
-    def test_public_default(self):
-        """ validate implementation doesn't somehow break default public access
-        """
-        self.assertEqual(self.baseclass.do_a_public_thing(), "public response")
-
-
-class TestPrivateInstanceMethodAccess(TestInstanceMethodAccessControl):
-
-    def test_raises_from_outside_class_scope(self):
-        """ validate AttributeError raised when attempting to access a private
-        method from outside class scope
-        """
-        with testtools.ExpectedException(AttributeError,
-                                         ".*has no attribute.*"):
-            self.baseclass.do_a_private_thing()
-
-        with testtools.ExpectedException(AttributeError,
-                                         ".*has no attribute.*"):
-            self.subclass.do_a_private_thing()
-
-    def test_raises_from_subclass(self):
-        """ validate AttributeError raised when attempting to access a private
-        method from subclass scope
-        """
-        with testtools.ExpectedException(AttributeError,
-                                         ".*has no attribute.*"):
-            self.subclass.do_another_private_thing_publicly()
-
-    def test_valid_from_within_class(self):
-        """ validate implementation doesn't somehow break base class access to
-        its own private methods
-        """
-        self.assertEqual(self.baseclass.do_a_private_thing_publicly(),
-                         "private response")
-
-    def test_valid_from_public_method_called_by_subclass(self):
-        """ validate implementation doesn't somehow break access to base class
-        private method when called from within public method in turn called by
-        sub class public method.
-        """
-        self.assertEqual(self.subclass.do_a_private_thing_publicly(),
-                         "private response")
-
-
-class TestProtectedInstanceMethodAccess(TestInstanceMethodAccessControl):
-
-    def test_raises_from_outside_class_scope(self):
-        """ validate AttributeError raised when attempting to access a
-        protected method directly from outside class scope
-        """
-        with testtools.ExpectedException(AttributeError,
-                                         ".*has no attribute.*"):
-            self.baseclass.do_a_protected_thing()
-
-    def test_raises_from_subclass(self):
-        """ validate AttributeError raised when attempting to access a
-        protected method directly from subclass scope
-        """
-        with testtools.ExpectedException(AttributeError,
-                                         ".*has no attribute.*"):
-            self.subclass.do_a_protected_thing()
-
-    def test_valid_from_within_class(self):
-        """ validate implementation doesn't somehow break base class access to
-        its own protected methods
-        """
-        self.assertEqual(self.baseclass.do_a_protected_thing_publicly(),
-                         "protected response")
-
-    def test_valid_from_public_method_called_by_subclass(self):
-        """ validate implementation doesn't somehow break access to base class
-        protected method when called from within public method in turn called
-        by sub class public method.
-        """
-        self.assertEqual(self.subclass.do_a_protected_thing_publicly(),
-                         "protected response")
-
-    def test_valid_from_protected_method_called_by_subclass(self):
-        """ validate implementation doesn't somehow break access to base class
-        protected method when called from within public method in turn called
-        by sub class public method.
-        """
-        self.assertEqual(self.subclass.do_another_protected_thing_publicly(),
-                         "protected response")
---
+++ tests/access/test_method_decorators_scenarios.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_method_decorators
+----------------------
+
+Tests for `access.private`, `access.protected`, and `access.public` decorators.
+All tests in this module assume that the default behavior of an undecorated
+method is to permit methods to be accessible on the object from any context.
+
+"""
+
+import inspect
+
+import access
+
+from tests.access.base import TestValidAccess, TestInvalidAccess
+
+
+def get_current_function_name():
+    outer_frame = inspect.stack[1]
+    return outer_frame.__name__
+
+
+class Base:
+
+    @access.private
+    def do_a_private_thing(self):
+        """Should raise AttributeError when called outside Base."""
+        return "%s response" % get_current_function_name()
+
+    @access.protected
+    def do_a_protected_thing(self):
+        """Should raise AttributeError when called outside Base or its sub
+        classes."""
+        return "%s response" % get_current_function_name()
+
+    def do_a_public_thing(self):
+        """Should be, like, totally fine all the time."""
+        return "%s response" % get_current_function_name()
+
+    def do_a_private_thing_publicly(self):
+        """Should be, like, totally fine all the time."""
+        return self.do_a_private_thing()
+
+    def do_a_protected_thing_publicly(self):
+        """Should be, like, totally fine all the time."""
+        return self.do_a_protected_thing()
+
+
+class Sub(Base):
+
+    def do_another_private_thing_publicly(self):
+        """Should raise AttributeError."""
+        return self.do_a_private_thing()
+
+    def do_another_protected_thing_publicly(self):
+        """Should be, like, totally fine all the time."""
+        return self.do_a_protected_thing()
+
+
+class SubTwo(Base):
+
+    def do_a_private_thing(self):
+        """Should be, like, totally fine all the time."""
+        return "%s response" % get_current_function_name()
+
+
+class TestValidBaseClassAccess(TestValidAccess):
+
+    scenarios = [
+        ("validate_explicitly_public_method",
+         {"method_name": "do_a_public_thing",
+          "fixture_class": Base,
+          "returns": "do_a_public_thing response",
+          }
+         ),
+        ("validate_public_method_call_private_method",
+         {"method_name": "do_a_private_thing_publicly",
+          "fixture_class": Base,
+          "returns": "do_a_private_thing response",
+          }
+         ),
+        ("validate_explicitly_public_method_call_private_method",
+         {"method_name": "do_a_protected_thing_publicly",
+          "fixture_class": Base,
+          "returns": "do_a_protected_thing response",
+          }
+         ),
+        ]
+
+
+class TestInvalidBaseClassAccess(TestInvalidAccess):
+
+    scenarios = [
+        ("private_method_inaccessible",
+         {"method_name": "do_a_private_thing",
+          "fixture_class": Base,
+          "exception": access.PrivateAttributeError,
+          }
+         ),
+        ("protected_method_inaccessible",
+         {"method_name": "do_a_protected_thing",
+          "fixture_class": Base,
+          "exception": access.ProtectedAttributeError,
+          }
+         ),
+        ]
+
+
+class TestValidSubClassAccess(TestValidAccess):
+
+    scenarios = [
+        ("validate_public_method_call_private_method",
+         {"method_name": "do_a_private_thing_publicly",
+          "fixture_class": Sub,
+          "returns": "do_a_private_thing response",
+          }
+         ),
+        ("base_protected_method_accessible_from_inside_subclass",
+         {"method_name": "do_another_protected_thing_publicly",
+          "fixture_class": Sub,
+          "returns": "do_a_protected_thing response",
+          }
+         ),
+        ("public_subclass_method_overrides_private_baseclass_method",
+         {"method_name": "do_a_private_thing",
+          "fixture_class": SubTwo,
+          "returns": "do_a_private_thing response",
+          }
+         ),
+        ]
+
+
+class TestInvalidSubClassAccess(TestInvalidAccess):
+
+    scenarios = [
+        ("base_private_method_inaccessible_from_outside_subclass",
+         {"method_name": "do_a_private_thing",
+          "fixture_class": Sub,
+          "exception": access.PrivateAttributeError,
+          }
+         ),
+        ("base_private_method_inaccessible_from_inside_subclass",
+         {"method_name": "do_another_private_thing_publicly",
+          "fixture_class": Sub,
+          "exception": access.PrivateAttributeError,
+          }
+         ),
+        ("base_protected_method_inaccessible_from_outside_subclass",
+         {"method_name": "do_a_protected_thing",
+          "fixture_class": Sub,
+          "exception": access.ProtectedAttributeError,
+          }
+         ),
+        ]
