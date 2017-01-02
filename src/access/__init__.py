1ed25c491e955d3240778f8d7c52ec40f279f566
Wayne Warren <waynr+launchpad@sdf.org>
2017-01-02 03:17:38 -0800

    Add noop implementation of basic functionality.
    Add noop function and class decorators used up to this point to to
    facilitate test case development.

--- src/access/__init__.py
+++ src/access/__init__.py
@@ -1,5 +1,53 @@
 # -*- coding: utf-8 -*-
 
+import functools
+
 __author__ = """Wayne Warren"""
 __email__ = 'waynr+python-access@sdf.org'
 __version__ = '0.0.0'
+
+
+PRIVATE = "private"
+PROTECTED = "protected"
+
+
+def private(function):
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        return function(*args, **kwargs)
+
+    return wrapper
+
+
+def protected(function):
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        return function(*args, **kwargs)
+
+    return wrapper
+
+
+def public(function):
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        return function(*args, **kwargs)
+
+    return wrapper
+
+
+def default(access_level):
+
+    def decorator(Cls):
+        return Cls
+    return decorator
+
+
+class PrivateAttributeError(AttributeError):
+    pass
+
+
+class ProtectedAttributeError(AttributeError):
+    pass
